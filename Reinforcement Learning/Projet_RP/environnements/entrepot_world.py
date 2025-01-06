152757feaafa7fe3e83ca127eb09399a026ca425
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2025-01-06 22:48:21 +0100

    typo

--- Reinforcement Learning/Projet_RP/environnements/entrepot_world.py
+++ Reinforcement Learning/Projet_RP/environnements/entrepot_world.py
@@ -8,20 +8,20 @@ class EntrepotWorldMDP:
         height: int,
         width: int,
         number_of_holes: int,
-        number_of_unclean_areas: int,
+        number_of_uncleaned_areas: int,
     ):
         self.height = height
         self.width = width
         self.number_of_holes = number_of_holes
-        self.number_of_unclean_areas = number_of_unclean_areas
+        self.number_of_uncleaned_areas = number_of_uncleaned_areas
 
-        # Create all possible states (position + unclean_areas)
+        # Create all possible states (position + uncleaned_areas)
         self.states = set(
             (i, j, tuple(combination))
             for i in range(height)
             for j in range(width)
             for combination in itertools.product(
-                [True, False], repeat=number_of_unclean_areas
+                [True, False], repeat=number_of_uncleaned_areas
             )
         )
 
@@ -47,18 +47,20 @@ class EntrepotWorldMDP:
         self.bad_states = [
             (i, j, tpl)
             for i, j in bad_states_pos
-            for tpl in itertools.product([True, False], repeat=number_of_unclean_areas)
+            for tpl in itertools.product(
+                [True, False], repeat=number_of_uncleaned_areas
+            )
         ]
 
         # Define the initial and terminal states
-        self.initial_state = (0, 0, tuple([False] * number_of_unclean_areas))
+        self.initial_state = (0, 0, tuple([False] * number_of_uncleaned_areas))
         self.terminal_state = (
             0,
             width - 1,
-            tuple([True] * number_of_unclean_areas),
+            tuple([True] * number_of_uncleaned_areas),
         )
 
-        # Define the unclean_area states
+        # Define the uncleaned_area states
         forbidden_states = self.bad_states + [self.initial_state, self.terminal_state]
 
         available_states = [
@@ -70,9 +72,9 @@ class EntrepotWorldMDP:
 
         unique_available_states = list({x[:2]: x for x in available_states}.keys())
 
-        self.unclean_area_states = [
+        self.uncleaned_area_states = [
             x[:2]
-            for x in random.sample(unique_available_states, number_of_unclean_areas)
+            for x in random.sample(unique_available_states, number_of_uncleaned_areas)
         ]
 
         # Define the transition probabilities and rewards
@@ -101,38 +103,38 @@ class EntrepotWorldMDP:
                     self.rewards[(state, action, new_state)] = -1.0
                 else:
                     self.transition_probabilities[(state, action, new_state)] = 1
-                    # If the new state is the terminal state (all unclean areas cleaned), then bonus
+                    # If the new state is the terminal state (all uncleaned areas cleaned), then bonus
                     if new_state == self.terminal_state:
                         self.rewards[(state, action, new_state)] = 3.0
-                    # If the new state is a new unclean area state, then bonus
-                    elif self.is_new_unclean_area_state(state, new_state):
+                    # If the new state is a new uncleaned area state, then bonus
+                    elif self.is_new_uncleaned_area_state(state, new_state):
                         self.rewards[(state, action, new_state)] = 2.0
-                    # If the agent goes to the terminal state without all the unclean_areas cleaned, then malus
+                    # If the agent goes to the terminal state without all the uncleaned_areas cleaned, then malus
                     elif new_state[:2] == self.terminal_state[:2]:
                         self.rewards[(state, action, new_state)] = -1.0
                     # Default case, no bonus or malus
                     else:
                         self.rewards[(state, action, new_state)] = 0
 
-    def is_new_unclean_area_state(self, state, new_state):
+    def is_new_uncleaned_area_state(self, state, new_state):
         for i in range(len(state[2])):
             if not state[2][i] and new_state[2][i]:
-                unclean_area_pos = self.unclean_area_states[i]
-                if new_state[:2] == unclean_area_pos:
+                uncleaned_area_pos = self.uncleaned_area_states[i]
+                if new_state[:2] == uncleaned_area_pos:
                     return True
         return False
 
     def take_action(self, state, action):
         new_state_pos = (state[0] + action[0], state[1] + action[1])
         new_state = (new_state_pos[0], new_state_pos[1], state[2])
-        for i in range(len(self.unclean_area_states)):
-            if new_state_pos == self.unclean_area_states[i]:
-                new_state_unclean_areas = list(state[2])
-                new_state_unclean_areas[i] = True
+        for i in range(len(self.uncleaned_area_states)):
+            if new_state_pos == self.uncleaned_area_states[i]:
+                new_state_uncleaned_areas = list(state[2])
+                new_state_uncleaned_areas[i] = True
                 new_state = (
                     new_state_pos[0],
                     new_state_pos[1],
-                    tuple(new_state_unclean_areas),
+                    tuple(new_state_uncleaned_areas),
                 )
         if new_state not in self.states or new_state in self.bad_states:
             new_state = state
@@ -153,9 +155,9 @@ class EntrepotWorldMDP:
                     cell = "T".center(cell_width)
                 elif any(state[0] == i and state[1] == j for state in self.bad_states):
                     cell = "X".center(cell_width)
-                elif (i, j) in self.unclean_area_states:
-                    for k in range(len(self.unclean_area_states)):
-                        if (i, j) == self.unclean_area_states[k]:
+                elif (i, j) in self.uncleaned_area_states:
+                    for k in range(len(self.uncleaned_area_states)):
+                        if (i, j) == self.uncleaned_area_states[k]:
                             cell = f"U{k}".center(cell_width)
                 else:
                     cell = ".".center(cell_width)
@@ -166,12 +168,12 @@ class EntrepotWorldMDP:
 
     def print_policy(self, policy: dict):
         iter_list = list(
-            itertools.product([True, False], repeat=self.number_of_unclean_areas)
+            itertools.product([True, False], repeat=self.number_of_uncleaned_areas)
         )
         reversed_iter_list = [tuple(reversed(item)) for item in reversed(iter_list)]
         print("Arrow order (U0, U1, ...):", reversed_iter_list)
 
-        cell_width = 3 * self.number_of_unclean_areas
+        cell_width = 3 * self.number_of_uncleaned_areas
         horizontal_border = "+" + ("-" * cell_width + "+") * self.width
 
         print(horizontal_border)
