f4ded69af2f8bfe263ace5168cca3c4ebc43dc35
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2025-01-10 11:37:32 +0100

    saliency cropation a gogo

--- AnalyseVideos/TP2/frame_extractor.py
+++ AnalyseVideos/TP2/frame_extractor.py
@@ -8,7 +8,7 @@ def extract_frames(video_path):
         return
 
     video_name = os.path.splitext(os.path.basename(video_path))[0]
-    output_dir = "Frames"
+    output_dir = "CroppedFrames"
     output_dir_path = os.path.join(os.path.dirname(video_path), output_dir)
 
     if not os.path.exists(output_dir_path):
@@ -32,9 +32,30 @@ def extract_frames(video_path):
         frame_filename = os.path.join(output_dir_path, f"frame_{frame_count:05d}.png")
         print(f"\rExtracting frame {frame_count}...", end="")
 
-        cv2.imwrite(frame_filename, frame)
+        saliency_map_path = os.path.join(
+            output_dir_path, "..", "SaliencyMaps", f"Saliency_{frame_count:05d}.png"
+        )
+        if not os.path.exists(saliency_map_path):
+            print(f"\nSaliency map for frame {frame_count} not found.")
+            continue
+
+        saliency_map = cv2.imread(saliency_map_path, cv2.IMREAD_GRAYSCALE)
+
+        cropped_png = crop_png_with_saliency_map(frame, saliency_map)
+        if cropped_png is not None:
+            cv2.imwrite(frame_filename, cropped_png)
 
         frame_count += 1
 
     cap.release()
     print(f"\nExtracted {frame_count} frames from '{video_name}'.")
+
+
+def crop_png_with_saliency_map(png, saliency_map):
+    if saliency_map.max() == 0:
+        return None
+
+    x, y, w, h = cv2.boundingRect(saliency_map)
+    cropped_png = png[y : y + h, x : x + w]
+
+    return cropped_png
