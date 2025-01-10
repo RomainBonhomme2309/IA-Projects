b05b7d5f464f88be37e7a164e37e9b4ae63439aa
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2025-01-10 10:14:40 +0100

    extractor

--- AnalyseVideos/TP2/main.py
+++ AnalyseVideos/TP2/main.py
@@ -3,11 +3,20 @@ import frame_extractor
 
 
 def extract_png_frames(video_directory):
+    total = 0
+    for root, _, files in os.walk(video_directory):
+        for file_name in files:
+            if file_name.endswith(".mp4"):
+                total += 1
+
+    print(f"Found {total} videos in '{video_directory}'.")
+
+    count = 1
     for root, _, files in os.walk(video_directory):
         for file_name in files:
             if file_name.endswith(".mp4"):
                 video_file = os.path.join(root, file_name)
-                print(f"Processing: {video_file}")
+                print(f"Processing: {video_file} ({count}/{total})")
                 frame_extractor.extract_frames(video_file)
 
 
